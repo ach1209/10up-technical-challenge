041ae2433bc2959a31ca689bc0afbbc52459caef
Anthony <ach0074@gmail.com>
2021-01-14 21:06:05 -0500

    Created content and footer sections. Added nav toggle event

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,18 @@
 import '../scss/styles.scss'
+
+const menu = document.getElementById('menu')
+let menuIcon = document.querySelector('.menu-icon')
+
+menu.addEventListener('click', menuToggle)
+
+function menuToggle(e) {
+  e.stopPropagation()
+
+  if (menuIcon.classList.contains('menu-icon--active')) {
+    menuIcon.classList.remove('menu-icon--active')
+    document.querySelector('.menu-list').removeAttribute('style')
+  } else {
+    menuIcon.classList.add('menu-icon--active')
+    document.querySelector('.menu-list').style.display = 'block'
+  }
+}
